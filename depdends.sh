bb4510b91c5bc8bb25f2e45d52a07ef1ea4d5d13
Saju <63098029+msaju@users.noreply.github.com>
2020-12-11 12:52:25 +0530

    Update depdends.sh

--- depdends.sh
+++ depdends.sh
@@ -1,5 +1,6 @@
-sudo apt-get install make automake autoconf libtool flex bison  \
-    pkg-config libssl-dev libxml2-dev python-dev libaio-dev       \
-    libibverbs-dev librdmacm-dev libreadline-dev liblvm2-dev      \
-    libglib2.0-dev liburcu-dev libcmocka-dev libsqlite3-dev       \
-    libacl1-dev uuid-dev
+sudo apt update
+sudo apt install -y docker.io
+systemctl start docker
+mkdir -p gluster-build
+cp Dockerfile gluster-build/
+cd gluster-build/
