b62613defe9a32737df760fdb8606b848bebb914
Saju <63098029+msaju@users.noreply.github.com>
2020-12-15 12:03:30 +0530

    Update startscript.sh

--- startscript.sh
+++ startscript.sh
@@ -5,8 +5,9 @@ cd github
 git config --global user.email "sajmoham@redhat.com"
 git config --global user.name  "msaju"
 git clone https://github.com/msaju/TEST_G.git
-rpm -ivh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 cd TEST_G
 ./autogen.sh
 ./configure
 make
+cd extras/LinuxRPM
+make glusterrpms
