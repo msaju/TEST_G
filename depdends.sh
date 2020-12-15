7c78720516a2aac711ebce5254bbe1f6226851bb
Saju <63098029+msaju@users.noreply.github.com>
2020-12-15 10:07:48 +0530

    Update depdends.sh

--- depdends.sh
+++ depdends.sh
@@ -1,7 +1,6 @@
 sudo apt update
 sudo apt install -y docker.io
 systemctl start docker
-git clone https://github.com/msaju/TEST_BUILD_AUTOMATION.git
 mkdir -p gluster-build
-cp TEST_BUILD_AUTOMATION/Dockerfile gluster-build/
+cp Dockerfile gluster-build/
 cd gluster-build/
